d3fed2ef8084c7602a06c8a4b2152bd093425eb5
ismatkl1 <ismavanden6@gmail.com>
2024-02-27 09:45:04 +0100

    spring

---
+++ demo/src/main/java/com/example/demo/LogController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogController {
+    private Logger log = LoggerFactory.getLogger(LogController.class);
+    @GetMapping("/page_log")
+    public String page(Model model) {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+        return "page";
+    }
+}
