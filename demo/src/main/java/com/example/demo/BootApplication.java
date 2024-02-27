8e099f987ebd9fa5d06491a2fc8f88a68d02b86c
miguelmnz14 <miguelmunozcano11@gmail.com>
2024-02-27 10:05:28 +0100

    hola

--- demo/src/main/java/com/example/demo/BootApplication.java
+++ demo/src/main/java/com/example/demo/BootApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
+        System.out.println("Hola");
     }
 }
