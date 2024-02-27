206b699345cd2999eec07e68a4ed43c76081597e
ismatkl1 <ismavanden6@gmail.com>
2024-02-27 10:22:58 +0100

    entrada controller

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootApplication {
+public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
         System.out.println("Hola");
     }
 }
