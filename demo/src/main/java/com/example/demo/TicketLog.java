42022e6d9777e3b9fd8a009ba0b3cee512ea202d
miguelmnz14 <miguelmunozcano11@gmail.com>
2024-03-09 13:27:45 +0100

    confirmations

--- demo/src/main/java/com/example/demo/TicketLog.java
+++ demo/src/main/java/com/example/demo/TicketLog.java
@@ -10,6 +10,9 @@ public class TicketLog {
     }
     @GetMapping("/about")
     public String about(){ return "about";}
+
+    @GetMapping("/confirmations")
+    public String confirmations(){ return "confirmations";}
 }
 
 
