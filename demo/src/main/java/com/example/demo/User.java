7f7650f45574453128c6433aa07f6dde4641e616
miguelmnz14 <miguelmunozcano11@gmail.com>
2024-03-10 20:36:35 +0100

    User

---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class User {
+    private String info;
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    public String getInfo() {
+        return info;
+    }
+}
+
