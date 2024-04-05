f79cf53ed6c2a1e16567d6d03659c8bbe13b6f8e
ismatkl1 <ismavanden6@gmail.com>
2024-04-05 11:41:12 +0200

    user

--- demo/src/main/java/com/example/demo/EventController.java
+++ demo/src/main/java/com/example/demo/EventController.java
@@ -44,9 +44,15 @@ public class EventController {
     }
     @PostConstruct
     public void init(){
-        Event event= new Event("Gran evento", "un gran evento", "Hoke", 20, 10);
+        Event event= new Event("Gran evento", "La pareja valenciana ha logrado ser la nueva sensación del hip-hop sin sello, sin publicidad, sin mánager, sin contacto con la prensa y con una discretísima presencia en redes gracias a 'BBO', un disco mimado al detalle y repleto de talento, una obra generacional ", "Hoke", 20, 10);
         event.setImage("image_imagenDWS.jpeg");
         eventService.save(event,null);
+        Event event1= new Event("Al Safir","Descripción: Diego Izquierdo, más conocido como “Al Safir”, es natural de la sierra de Madrid (1995). Su inquietud musical le ha llevado a experimentar un sonido atípico y configurarse como la promesa del underground nacional. Al Safir desvela ‘Black Ops’, el segundo adelanto de su próximo disco. El single comparte título con su nuevo álbum y supone un salto hacia sonidos más experimentales dentro del género urbano.","Diego Izquierdo",20,200);
+        event1.setImage("image_WhatsApp Image 2024-03-12 at 09.10.52.jpeg");
+        eventService.save(event1,null);
+        Event event2=new Event("Kid Keo","Algunos de tus temas superan los diez millones de reproducciones en Spotify, como Kikiki o Ma Vie. Por no hablar de Dracukeo, tu single más célebre, cuyo vídeo supera las noventa millones de visitas en YouTube.","Padua Keoma",30,250);
+        event2.setImage("image_WhatsApp Image 2024-03-12 at 09.19.15.jpeg");
+        eventService.save(event2,null);
         if (user==null){
             user=new User("peterparker",null);
             user.setMyEvents(new ArrayList<>());
@@ -101,7 +107,7 @@ public class EventController {
     }
     @PostMapping("/events/{id}/comments")
     public String submitComment(Model model, String username, String content, int valoration, @PathVariable long id) {
-        Comment comment = new Comment(this.user.getUsername(), content, valoration);
+        Comment comment = new Comment(username, content, valoration);
         comment.setEventId(id);
         Event event =eventService.findById(id);
         eventService.addComment(event, comment);
