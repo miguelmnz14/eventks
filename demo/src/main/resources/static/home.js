a912a2d67cc8c3f6957006aeb2ea090fb9dbcd09
miguelmnz14 <miguelmunozcano11@gmail.com>
2024-04-10 20:52:54 +0200

    editor

--- demo/src/main/resources/static/home.js
+++ demo/src/main/resources/static/home.js
@@ -9,5 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+var quill = new Quill('#editor', {
+    theme: 'snow'  // Tema de estilo del editor (puedes cambiarlo a 'bubble' si prefieres un estilo de burbuja)
+});
+
+var contenido = quill.root.innerHTML;
+console.log(contenido);
+
+function actualizarTexto() {
+    var texto = quill.root.innerHTML; // Obtiene el contenido HTML del editor
+    document.getElementById('texto-actualizado').innerHTML = texto; // Actualiza el contenido del elemento en la página
+}
+
 
 
