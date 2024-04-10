f449722e9b5f35147bfbde9ecbba46a93d692754
ismatkl1 <ismavanden6@gmail.com>
2024-04-11 00:28:16 +0200

    c

--- demo/src/main/java/com/example/demo/BBDDController.java
+++ demo/src/main/java/com/example/demo/BBDDController.java
@@ -5,16 +5,26 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BBDDController {
     @Autowired
     private EventRepository eventRepository;
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private UserRepository userRepository;
     @PostConstruct
     public void init(){
         Event event=new Event("pet","jajaj","kka",1,3);
+        event.setImage("null");
         eventRepository.save(event);
+        List <Event> eventi=new ArrayList<>();
+        User user=new User("peter",eventi);
+        userRepository.save(user);
     }
 
 
